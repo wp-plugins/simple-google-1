fece73ae4743d673918a3c590c627ba926057087
Edodi <Edodi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-15 08:52:37 +0000

    Version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/simple-google-1/trunk@451300 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: simple Google +1
+Plugin URI: http://www.lightapps.co.il
+Description: Use this plugin in order to add a Google +1 button to each and every post. 
+Author: Edo Frenkel
+Version: 1.0
+Author URI: http://www.lightapps.co.il
+*/ 
+
+define ('GOOGLE_ADD_TEMPLATE',"<g:plusone size=\"tall\" annotation=\"inline\"></g:plusone>");
+define ('GOOGLE_ADD_JS',"<script type=\"text/javascript\">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>");
+
+function google_get_button($content) 
+{
+	return $content . GOOGLE_ADD_TEMPLATE;
+}
+function google_add_js() 
+{
+	print GOOGLE_ADD_JS;
+}
+add_action('wp_head','google_add_js');
+add_filter('the_content', 'google_get_button');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== simple Google +1 ===
+Contributors: Edo Frenkel
+Tags: google, 1+
+Requires at least: 1.0
+Tested up to: 3.2.2
+Stable tag: 1.0
+
+== Description ==
+insert Google +1 button to the ent of you posts.
+
+== Installation ==
+
+1. Download the file 'simple_google_button.zip'
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+that's it...
+
+Manually:
+
+1. Upload `j14-updates` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+1. Screenshot.png
+
+== Changelog ==
